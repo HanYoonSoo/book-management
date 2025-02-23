d2355257a80354bbe7df7d8a4918a9f68ad0270d
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-23 17:27:25 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/com/hanyoonsoo/bookmanagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanyoonsoo.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
