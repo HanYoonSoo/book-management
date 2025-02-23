8fdd17236262870bd3e903a5b4e941491c75a7e8
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-23 19:10:01 +0900

    [#3] feat: SwaggerConfig 설정

---
+++ src/main/java/com/hanyoonsoo/bookmanagement/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.hanyoonsoo.bookmanagement.global.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    static {
+        io.swagger.v3.core.jackson.ModelResolver.enumsAsRef = true;
+    }
+
+    @Bean
+    public GroupedOpenApi v1OpenAPI() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/api/v1/**")
+                .addOpenApiCustomizer(openApi -> openApi.info(v1Info()))
+                .build();
+    }
+
+    private Info v1Info() {
+        return new Info()
+                .title("Book Management System API")
+                .version("1.0")
+                .description("API version 1 documentation");
+    }
+}
