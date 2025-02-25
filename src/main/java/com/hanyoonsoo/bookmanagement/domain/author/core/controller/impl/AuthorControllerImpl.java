af9c093404083810cbc375ed4b3eabf058739120
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-25 23:23:06 +0900

    [#9] feat: 저자 수정 컨트롤러 메서드 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/author/core/controller/impl/AuthorControllerImpl.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/author/core/controller/impl/AuthorControllerImpl.java
@@ -2,13 +2,12 @@ package com.hanyoonsoo.bookmanagement.domain.author.core.controller.impl;
 
 import com.hanyoonsoo.bookmanagement.domain.author.core.controller.AuthorController;
 import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.CreateAuthorRequest;
+import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.UpdateAuthorRequest;
 import com.hanyoonsoo.bookmanagement.domain.author.core.service.AuthorService;
 import com.hanyoonsoo.bookmanagement.global.common.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +16,21 @@ public class AuthorControllerImpl implements AuthorController {
     private final AuthorService authorService;
 
     @Override
-    @RequestMapping("/authors")
+    @PostMapping("/authors")
     public ApiResponse<Void> create(@Valid @RequestBody CreateAuthorRequest request) {
         authorService.create(request);
 
         return ApiResponse.created(null);
     }
+
+    @Override
+    @PatchMapping("/authors/{authorId}")
+    public ApiResponse<Void> update(
+            @Valid @RequestBody UpdateAuthorRequest request, @PathVariable("authorId") Long authorId
+    ) {
+        authorService.update(request, authorId);
+
+        return ApiResponse.ok(null);
+    }
+
 }
