b3139b143533020e1fcc67472685902d6e775bb7
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-25 20:59:03 +0900

    [#9] feat: 저자 JpaRepository 테스트 작성
        reword 7d5169f [#9] test: 저자 JpaRepository 테스트 작성
        reword c16be31 [#9] test: 저자 생성 비즈니스 로직 테스트 작성

---
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/repository/AuthorRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.hanyoonsoo.bookmanagement.domain.author.core.repository;
+
+import com.hanyoonsoo.bookmanagement.domain.author.core.entity.Author;
+import com.hanyoonsoo.bookmanagement.domain.author.core.repository.jpa.AuthorJpaRepository;
+import com.hanyoonsoo.bookmanagement.global.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import(JpaConfig.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class AuthorRepositoryTest {
+
+    @Autowired
+    private AuthorJpaRepository authorJpaRepository;
+
+    @Test
+    @DisplayName("이메일이 중복되지 않으면, 저자 생성에 성공한다.")
+    void emailNotDuplicated_thenSaveSuccess() throws Exception {
+        //given
+        Author author = Author.builder()
+                .name("홍길동")
+                .email("test@example.com")
+                .build();
+
+        //when & then
+        assertDoesNotThrow(() -> authorJpaRepository.save(author));
+    }
+
+    @Test
+    @DisplayName("이메일이 중복이면, 저자 생성에 실패한다.")
+    void emailDuplicated_thenSaveFail() throws Exception {
+        //given
+        Author author = Author.of("홍길동", "test-fail@example.com");
+        authorJpaRepository.save(author);
+
+        //when & then
+        assertThrows(DataIntegrityViolationException.class,
+                () -> authorJpaRepository.save(Author.of("홍길동", "test-fail@example.com")));
+    }
+}
