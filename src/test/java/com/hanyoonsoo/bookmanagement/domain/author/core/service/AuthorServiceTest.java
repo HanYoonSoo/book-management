4aaaad8b3067eedf5194f36b1a0778f958579bad
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-25 23:26:10 +0900

    [#9] test: 저자 수정 비즈니스 로직 테스트 작성

--- src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/service/AuthorServiceTest.java
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/service/AuthorServiceTest.java
@@ -1,6 +1,7 @@
 package com.hanyoonsoo.bookmanagement.domain.author.core.service;
 
 import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.CreateAuthorRequest;
+import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.UpdateAuthorRequest;
 import com.hanyoonsoo.bookmanagement.domain.author.core.entity.Author;
 import com.hanyoonsoo.bookmanagement.domain.author.core.exception.AuthorException;
 import com.hanyoonsoo.bookmanagement.domain.author.core.repository.AuthorRepository;
@@ -23,7 +24,7 @@ class AuthorServiceTest {
     }
 
     @Test
-    @DisplayName("이메일이 중복되지 않았을 경우 저자 생성에 성공한다.")
+    @DisplayName("이메일이 중복되지 않았다면, 저자 생성에 성공한다.")
     void create_whenEmailNotDuplicated_thenSuccess() throws Exception {
         // given
         CreateAuthorRequest request = CreateAuthorRequest.builder()
@@ -36,7 +37,7 @@ class AuthorServiceTest {
     }
 
     @Test
-    @DisplayName("이메일이 중복되었을 경우 저자 생성에 실패한다.")
+    @DisplayName("이메일이 중복되었다면, 저자 생성에 실패한다.")
     void create_whenEmailDuplicated_thenFail() throws Exception {
         // given
         Author author = Author.of("홍길동", "test@example.com");
@@ -49,5 +50,70 @@ class AuthorServiceTest {
 
         // when & then
         assertThrows(AuthorException.class, () -> authorService.create(request));
+    }
+
+    @Test
+    @DisplayName("이메일이 중복되지 않고 Author Id가 존재한다면, 저자 수정에 성공한다.")
+    void update_whenEmailNotDuplicatedAndExistsAuthorId_thenSuccess(){
+        //given
+        Author author = Author.of("홍길동", "test@example.com");
+        long authorId = MemoryAuthorRepository.authorId.get();
+
+        authorRepository.save(author);
+
+        UpdateAuthorRequest request = UpdateAuthorRequest.builder()
+                .name("홍길동수정")
+                .email("test-update@example.com")
+                .build();
+
+        //when
+        authorService.update(request, authorId);
+
+        //then
+        Author findAuthor = authorRepository.findByIdOrElseThrow(authorId);
+        assertEquals(findAuthor.getName(), request.getName());
+        assertEquals(findAuthor.getEmail(), request.getEmail());
+    }
+
+    @Test
+    @DisplayName("이메일이 중복되었다면, 저자 수정에 실패한다.")
+    void update_whenEmailDuplicated_thenFail(){
+        //given
+        Author author1 = Author.of("홍길동1", "test@example.com");
+        long authorId = MemoryAuthorRepository.authorId.get();
+
+        Author author2 = Author.of("홍길동2", "test-update@example.com");
+
+        authorRepository.save(author1);
+        authorRepository.save(author2);
+
+        UpdateAuthorRequest request = UpdateAuthorRequest.builder()
+                .name("홍길동수정")
+                .email("test-update@example.com")
+                .build();
+
+        //when & then
+        assertThrows(AuthorException.class, () -> authorService.update(request, authorId));
+    }
+
+    @Test
+    @DisplayName("전달받은 Id의 Author가 존재하지 않으면, 저자 수정에 실패한다.")
+    void update_whenNotFoundAuthor_thenFail(){
+        //given
+        Author author1 = Author.of("홍길동1", "test@example.com");
+        long authorId = MemoryAuthorRepository.authorId.get();
+
+        Author author2 = Author.of("홍길동2", "test-update@example.com");
+
+        authorRepository.save(author1);
+        authorRepository.save(author2);
+
+        UpdateAuthorRequest request = UpdateAuthorRequest.builder()
+                .name("홍길동수정")
+                .email("test-update@example.com")
+                .build();
+
+        //when & then
+        assertThrows(AuthorException.class, () -> authorService.update(request, authorId));
     }
 }
--- src/test/java/com/hanyoonsoo/bookmanagement/domain/author/fake/MemoryAuthorRepository.java
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/author/fake/MemoryAuthorRepository.java
@@ -1,24 +1,35 @@
 package com.hanyoonsoo.bookmanagement.domain.author.fake;
 
 import com.hanyoonsoo.bookmanagement.domain.author.core.entity.Author;
+import com.hanyoonsoo.bookmanagement.domain.author.core.exception.AuthorException;
 import com.hanyoonsoo.bookmanagement.domain.author.core.repository.AuthorRepository;
+import com.hanyoonsoo.bookmanagement.global.common.enums.ErrorCode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MemoryAuthorRepository implements AuthorRepository {
 
     private final Map<Long, Author> authors = new HashMap<>();
-    private Long authorId = 1L;
+    public static final AtomicLong authorId = new AtomicLong(1);
+
 
     @Override
     public void save(Author author) {
-        authors.put(authorId++, author);
+        authors.put(authorId.getAndAdd(1), author);
     }
 
     @Override
     public boolean existsByEmail(String email) {
         return authors.values().stream()
                 .anyMatch(author -> author.getEmail().equals(email));
+    }
+
+    @Override
+    public Author findByIdOrElseThrow(Long authorId) {
+        return Optional.ofNullable(authors.get(authorId))
+                .orElseThrow(() -> new AuthorException(ErrorCode.NOT_FOUND, "Not Found Author"));
     }
 }
