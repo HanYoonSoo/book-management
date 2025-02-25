b4418760d20fa4b1b4021f2a8c982256087eda26
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-25 23:26:26 +0900

    [#9] test: 저자 수정 API 컨트롤러 테스트 코드 작성

--- src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/controller/AuthorControllerTest.java
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/controller/AuthorControllerTest.java
@@ -2,6 +2,7 @@ package com.hanyoonsoo.bookmanagement.domain.author.core.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.CreateAuthorRequest;
+import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.UpdateAuthorRequest;
 import com.hanyoonsoo.bookmanagement.domain.author.core.service.AuthorService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,9 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,12 +43,33 @@ class AuthorControllerTest {
 
         doNothing()
                 .when(authorService)
-                .create(request);
+                .create(any());
 
         mockMvc.perform(post("/authors")
                 .contentType("application/json")
                 .content(requestBody))
                 .andDo(print())
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("저자 수정에 성공하면 200 상태코드를 전달 받는다.")
+    void update_whenSuccess_thenReturnStatus200() throws Exception {
+        UpdateAuthorRequest request = UpdateAuthorRequest.builder()
+                .name("홍길도수정")
+                .email("test-update@example.com")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        doNothing()
+                .when(authorService)
+                .update(any(), any());
+
+        mockMvc.perform(patch("/authors/1")
+                .contentType("application/json")
+                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
