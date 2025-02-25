af7e582387dc5681489655a3c632ad1a5fa5a131
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-25 23:19:10 +0900

    [#9] feat: ErrorCode에 중복 경우 추가

--- src/main/java/com/hanyoonsoo/bookmanagement/global/common/enums/ErrorCode.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/global/common/enums/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "Not Found"),
     FORBIDDEN(HttpStatus.FORBIDDEN, "Forbidden"),
     NOT_VALID(HttpStatus.BAD_REQUEST, "Not Valid"),
+    DUPLICATED(HttpStatus.CONFLICT, "Duplicated"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
 
     private final HttpStatus httpStatus;
