904ec0bf951854e09ae3b9b550c8baaa44f0f20b
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-26 00:38:27 +0900

    [#9] test: 저자 컨트롤러 테스트 given/when/then 적용

--- src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/controller/AuthorControllerTest.java
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/author/core/controller/AuthorControllerTest.java
@@ -3,6 +3,7 @@ package com.hanyoonsoo.bookmanagement.domain.author.core.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.CreateAuthorRequest;
 import com.hanyoonsoo.bookmanagement.domain.author.core.dto.request.UpdateAuthorRequest;
+import com.hanyoonsoo.bookmanagement.domain.author.core.dto.response.GetAuthorResponse;
 import com.hanyoonsoo.bookmanagement.domain.author.core.service.AuthorService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthorController.class)
@@ -33,6 +38,7 @@ class AuthorControllerTest {
     @Test
     @DisplayName("저자 생성에 성공하면 201 상태코드를 전달 받는다.")
     void create_whenSuccess_thenReturnStatus201() throws Exception {
+        //given
         CreateAuthorRequest request = CreateAuthorRequest.builder()
                 .name("홍길동")
                 .email("test@example.com")
@@ -44,6 +50,7 @@ class AuthorControllerTest {
                 .when(authorService)
                 .create(any());
 
+        //when & then
         mockMvc.perform(post("/authors")
                 .contentType("application/json")
                 .content(requestBody))
@@ -54,6 +61,7 @@ class AuthorControllerTest {
     @Test
     @DisplayName("저자 수정에 성공하면 200 상태코드를 전달 받는다.")
     void update_whenSuccess_thenReturnStatus200() throws Exception {
+        //given
         UpdateAuthorRequest request = UpdateAuthorRequest.builder()
                 .name("홍길도수정")
                 .email("test-update@example.com")
@@ -65,6 +73,7 @@ class AuthorControllerTest {
                 .when(authorService)
                 .update(any(), any());
 
+        //when & then
         mockMvc.perform(patch("/authors/1")
                 .contentType("application/json")
                 .content(requestBody))
@@ -75,22 +84,49 @@ class AuthorControllerTest {
     @Test
     @DisplayName("저자 목록 조회에 성공하면 200 상태코드를 전달 받는다.")
     void readAuthors_whenSuccess_thenReturnStatus200() throws Exception {
-        doNothing()
-                .when(authorService)
-                .readAuthors();
+        //given
+        List<GetAuthorResponse> response = List.of(
+                GetAuthorResponse.builder()
+                        .authorId(1L)
+                        .name("홍길동1")
+                        .email("test1@example.com")
+                        .build(),
+                GetAuthorResponse.builder()
+                        .authorId(2L)
+                        .name("홍길동2")
+                        .email("test2@example.com")
+                        .build()
+        );
 
+        given(authorService.readAuthors()).willReturn(response);
+
+        //when & then
         mockMvc.perform(get("/authors"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.length()").value(2))
+                .andExpect(jsonPath("$.data[0].authorId").value(1L))
+                .andExpect(jsonPath("$.data[0].name").value("홍길동1"))
+                .andExpect(jsonPath("$.data[0].email").value("test1@example.com"))
+                .andExpect(jsonPath("$.data[1].authorId").value(2L))
+                .andExpect(jsonPath("$.data[1].name").value("홍길동2"))
+                .andExpect(jsonPath("$.data[1].email").value("test2@example.com"));
     }
 
     @Test
     @DisplayName("저자 상세 조회에 성공하면 200 상태코드를 전달 받는다.")
     void readAuthorDetail_whenSuccess_thenReturnStatus200() throws Exception {
-        doNothing()
-                .when(authorService)
-                .readAuthorDetail(any());
+        //given
+        GetAuthorResponse response = GetAuthorResponse.builder()
+                .authorId(1L)
+                .name("홍길동1")
+                .email("test1@example.com")
+                .build();
 
+        given(authorService.readAuthorDetail(1L)).willReturn(response);
+
+        //when & then
         mockMvc.perform(get("/authors/1"))
                 .andDo(print())
                 .andExpect(status().isOk());
