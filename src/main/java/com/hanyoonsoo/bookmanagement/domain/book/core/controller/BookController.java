451673255d884222735e9546f7fe37b9d395ec7b
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-26 18:03:53 +0900

    [#10] feat: 도서 저장 API 컨트롤러 메서드 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/BookController.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/BookController.java
@@ -1,4 +1,9 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.controller;
 
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
+import com.hanyoonsoo.bookmanagement.global.common.dto.ApiResponse;
+
 public interface BookController {
+
+    ApiResponse<Void> create(CreateBookRequest request);
 }
--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/impl/BookControllerImpl.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/impl/BookControllerImpl.java
@@ -1,8 +1,13 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.controller.impl;
 
 import com.hanyoonsoo.bookmanagement.domain.book.core.controller.BookController;
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
 import com.hanyoonsoo.bookmanagement.domain.book.core.service.BookService;
+import com.hanyoonsoo.bookmanagement.global.common.dto.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookControllerImpl implements BookController {
 
     private final BookService bookService;
+
+    @Override
+    @PostMapping("/books")
+    public ApiResponse<Void> create(@Valid @RequestBody CreateBookRequest request) {
+        bookService.create(request);
+
+        return ApiResponse.created(null);
+    }
 }
