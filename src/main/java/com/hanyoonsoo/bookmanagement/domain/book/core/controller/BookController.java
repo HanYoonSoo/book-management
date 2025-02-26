a496665569335f09f8a00025bee61cd4e5c525a2
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-26 19:16:12 +0900

    [#10] docs: 도서 생성 API 문서 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/BookController.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/BookController.java
@@ -2,8 +2,24 @@ package com.hanyoonsoo.bookmanagement.domain.book.core.controller;
 
 import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
 import com.hanyoonsoo.bookmanagement.global.common.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 
 public interface BookController {
 
+    @Operation(
+            summary = "Book 생성",
+            description = "Book 생성 API, isbn 중복되지 않았다면 Book 생성에 성공합니다. " +
+                    "존재하지 않는 Author Id일 경우 404 에러를 반환합니다. " +
+                    "이미 존재하는 isbn일 경우 생성에 실패하고 409 에러를 반환합니다."
+    )
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "201", description = "Book 생성 성공"
+    )
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "404", description = "존재하지 않는 Author Id일 경우 Book 생성 실패"
+    )
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "409", description = "이미 존재하는 isbn으로 인해 Book 생성 실패"
+    )
     ApiResponse<Void> create(CreateBookRequest request);
 }
