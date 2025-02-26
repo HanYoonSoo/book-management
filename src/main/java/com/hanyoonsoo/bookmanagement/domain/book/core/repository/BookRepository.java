f3be3592b01efc3baf5b6e8274a91a83d49da345
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-26 18:02:17 +0900

    [#10] feat: 도서 저장 및 Isbn 존재 확인 메소드 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/BookRepository.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.repository;
 
+import com.hanyoonsoo.bookmanagement.domain.book.core.entity.Book;
+
 public interface BookRepository {
+
+    void save(Book book);
+
+    boolean existsByIsbn(String isbn);
 }
--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/impl/BookRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.repository.impl;
 
+import com.hanyoonsoo.bookmanagement.domain.book.core.entity.Book;
 import com.hanyoonsoo.bookmanagement.domain.book.core.repository.BookRepository;
 import com.hanyoonsoo.bookmanagement.domain.book.core.repository.jpa.BookJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +11,14 @@ import org.springframework.stereotype.Repository;
 public class BookRepositoryImpl implements BookRepository {
 
     private final BookJpaRepository bookJpaRepository;
+
+    @Override
+    public void save(Book book) {
+        bookJpaRepository.save(book);
+    }
+
+    @Override
+    public boolean existsByIsbn(String isbn) {
+        return bookJpaRepository.existsByIsbn(isbn);
+    }
 }
