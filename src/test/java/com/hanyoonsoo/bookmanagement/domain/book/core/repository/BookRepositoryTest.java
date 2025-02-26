2be7c2ca91a557c93f28499d91aa3fa069fa5127
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-27 05:26:09 +0900

    [#10] test: 도서 수정 관련 JpaRepository 테스트 작성

--- src/test/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/BookRepositoryTest.java
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/BookRepositoryTest.java
@@ -30,7 +30,7 @@ class BookRepositoryTest {
 
     @Test
     @DisplayName("isbn이 중복되지 않으면, 도서 생성에 성공한다.")
-    void isbnNotDuplicated_thenSaveSuccess() throws Exception {
+    void whenIsbnNotDuplicated_thenSaveSuccess() throws Exception {
         //given
         Author author = Author.builder()
                 .name("홍길동")
@@ -53,7 +53,7 @@ class BookRepositoryTest {
 
     @Test
     @DisplayName("isbn이 중복되면, 도서 생성에 실패한다.")
-    void isbnDuplicated_thenSaveFail() throws Exception {
+    void whenIsbnDuplicated_thenSaveFail() throws Exception {
         //given
         Author author = Author.builder()
                 .name("홍길동")
@@ -82,5 +82,54 @@ class BookRepositoryTest {
                         author)
                 )
         );
+    }
+
+    @Test
+    @DisplayName("도서의 저자가 존재하지 않는다면, 도서 생성에 실패한다.")
+    void whenNotFoundBook_thenSaveFail() throws Exception {
+        //given
+        Book book = Book.of(
+                "테스트 타이틀",
+                "테스트 설명",
+                "1234567890",
+                LocalDate.now(),
+                null
+        );
+
+        //when & then
+        assertThrows(DataIntegrityViolationException.class,
+                () -> bookJpaRepository.save(book)
+        );
+    }
+
+    @Test
+    @DisplayName("존재하는 도서이며 isbn이 중복되지 않았다면, JPA의 더티 체킹을 사용한 도서 수정에 성공한다.")
+    void whenExistsBookAndNotDuplicatedIsbn_thenJpaDirtyCheckingUpdateSuccess() throws Exception {
+        //given
+        Author author = Author.builder()
+                .name("홍길동")
+                .email("test@example.com")
+                .build();
+
+        authorJpaRepository.save(author);
+
+        Book book = Book.of(
+                "테스트 타이틀",
+                "테스트 설명",
+                "1234567890",
+                LocalDate.now(),
+                author
+        );
+
+        Book savedBook = bookJpaRepository.save(book);
+
+        //when
+        savedBook.modifyIsbn("2345678980");
+        savedBook.modifyTitle("테스트 수정 타이틀");
+
+        //then
+        Book updatedBook = bookJpaRepository.findById(savedBook.getId()).orElse(null);
+        assertEquals("2345678980", updatedBook.getIsbn());
+        assertEquals("테스트 수정 타이틀", updatedBook.getTitle());
     }
 }
