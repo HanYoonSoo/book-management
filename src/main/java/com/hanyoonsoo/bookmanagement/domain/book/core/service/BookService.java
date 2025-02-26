5bc205b76d6194110161aca501fa68a132b1afd5
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-27 05:22:56 +0900

    [#10] feat: 도서 수정 비즈니스 로직 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/BookService.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/BookService.java
@@ -1,8 +1,11 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.service;
 
 import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.UpdateBookRequest;
 
 public interface BookService {
 
     void create(CreateBookRequest request);
+
+    void update(UpdateBookRequest request, Long bookId);
 }
--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/impl/BookServiceImpl.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/impl/BookServiceImpl.java
@@ -3,6 +3,7 @@ package com.hanyoonsoo.bookmanagement.domain.book.core.service.impl;
 import com.hanyoonsoo.bookmanagement.domain.author.core.entity.Author;
 import com.hanyoonsoo.bookmanagement.domain.author.core.service.AuthorService;
 import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.UpdateBookRequest;
 import com.hanyoonsoo.bookmanagement.domain.book.core.entity.Book;
 import com.hanyoonsoo.bookmanagement.domain.book.core.exception.BookException;
 import com.hanyoonsoo.bookmanagement.domain.book.core.repository.BookRepository;
@@ -10,6 +11,11 @@ import com.hanyoonsoo.bookmanagement.domain.book.core.service.BookService;
 import com.hanyoonsoo.bookmanagement.global.common.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +25,7 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     @Override
+    @Transactional
     public void create(CreateBookRequest request) {
         Author author = authorService.validateExistsAuthor(request.getAuthorId());
         validateDuplicatedIsbn(request.getIsbn());
@@ -27,11 +34,35 @@ public class BookServiceImpl implements BookService {
                 request.getTitle(),
                 request.getDescription(),
                 request.getIsbn(),
-                request.getPublication_date(),
+                request.getPublicationDate(),
                 author
         );
 
         bookRepository.save(book);
+    }
+
+    @Override
+    @Transactional
+    public void update(UpdateBookRequest request, Long bookId) {
+        Book book = validateExistsBook(bookId);
+
+        String isbn = request.getIsbn();
+        String title = request.getTitle();
+        String description = request.getDescription();
+        LocalDate publicationDate = request.getPublicationDate();
+
+        if(hasText(isbn)){
+            validateDuplicatedIsbn(isbn);
+            book.modifyIsbn(isbn);
+        }
+
+        if(hasText(title)) book.modifyTitle(title);
+        if(hasText(description)) book.modifyDescription(description);
+        if(publicationDate != null) book.modifyPublicationDate(publicationDate);
+    }
+
+    private Book validateExistsBook(Long bookId) {
+        return bookRepository.findByIdOrElseThrow(bookId);
     }
 
     private void validateDuplicatedIsbn(String isbn) {
