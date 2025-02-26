c1fadef8c9ff91567c86a1900a9912cb3e0e25dd
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-26 18:01:55 +0900

    [#10] feat: Isbn 존재 확인 Query Method 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/jpa/BookJpaRepository.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/repository/jpa/BookJpaRepository.java
@@ -4,4 +4,6 @@ import com.hanyoonsoo.bookmanagement.domain.book.core.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookJpaRepository extends JpaRepository<Book, Long> {
+
+    boolean existsByIsbn(String isbn);
 }
