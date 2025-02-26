6db9d5013d8e3353ceda2af01fb51a3fe2c72ba2
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-26 18:02:46 +0900

    [#10] feat: 도서 저장 비즈니스 로직 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/BookService.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/BookService.java
@@ -1,4 +1,8 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.service;
 
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
+
 public interface BookService {
+
+    void create(CreateBookRequest request);
 }
--- src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/impl/BookServiceImpl.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/book/core/service/impl/BookServiceImpl.java
@@ -1,7 +1,13 @@
 package com.hanyoonsoo.bookmanagement.domain.book.core.service.impl;
 
+import com.hanyoonsoo.bookmanagement.domain.author.core.entity.Author;
+import com.hanyoonsoo.bookmanagement.domain.author.core.service.AuthorService;
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
+import com.hanyoonsoo.bookmanagement.domain.book.core.entity.Book;
+import com.hanyoonsoo.bookmanagement.domain.book.core.exception.BookException;
 import com.hanyoonsoo.bookmanagement.domain.book.core.repository.BookRepository;
 import com.hanyoonsoo.bookmanagement.domain.book.core.service.BookService;
+import com.hanyoonsoo.bookmanagement.global.common.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +15,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
+    private final AuthorService authorService;
     private final BookRepository bookRepository;
+
+    @Override
+    public void create(CreateBookRequest request) {
+        Author author = authorService.validateExistsAuthor(request.getAuthorId());
+        validateDuplicatedIsbn(request.getIsbn());
+
+        Book book = Book.of(
+                request.getTitle(),
+                request.getDescription(),
+                request.getIsbn(),
+                request.getPublication_date(),
+                author
+        );
+
+        bookRepository.save(book);
+    }
+
+    private void validateDuplicatedIsbn(String isbn) {
+        boolean exists = bookRepository.existsByIsbn(isbn);
+
+        if(exists) throw new BookException(ErrorCode.DUPLICATED, "Already Exists Isbn");
+    }
 }
