78fa9b9fe365d4439c17d07c3c0293dd64382bfb
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-27 05:26:24 +0900

    [#10] test: 도서 수정 API 컨트롤러 테스트 작성

--- src/test/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/BookControllerTest.java
+++ src/test/java/com/hanyoonsoo/bookmanagement/domain/book/core/controller/BookControllerTest.java
@@ -2,6 +2,7 @@ package com.hanyoonsoo.bookmanagement.domain.book.core.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.CreateBookRequest;
+import com.hanyoonsoo.bookmanagement.domain.book.core.dto.request.UpdateBookRequest;
 import com.hanyoonsoo.bookmanagement.domain.book.core.service.BookService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -97,6 +99,56 @@ class BookControllerTest {
 
         //when & then
         mockMvc.perform(post("/books")
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("도서 수정에 성공하면, 200 상태코드를 전달 받는다.")
+    void update_whenSuccess_thenReturnStatus201() throws Exception {
+        //given
+        UpdateBookRequest request = UpdateBookRequest.builder()
+                .title("테스트 수정 타이틀")
+                .description("테스트 수정 설명")
+                .publicationDate(LocalDate.now())
+                .isbn("2345678980")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        doNothing()
+                .when(bookService)
+                .update(any(), any());
+
+        //when & then
+        mockMvc.perform(patch("/books/1")
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("isbn 형식이 잘못되었다면, 400 상태코드를 전달 받는다.")
+    void update_whenNotValidIsbn_thenReturnStatus400() throws Exception {
+        //given
+        UpdateBookRequest request = UpdateBookRequest.builder()
+                .title("테스트 수정 타이틀")
+                .description("테스트 수정 설명")
+                .publicationDate(LocalDate.now())
+                .isbn("2345678981")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        doNothing()
+                .when(bookService)
+                .update(any(), any());
+
+        //when & then
+        mockMvc.perform(patch("/books/1")
                         .contentType("application/json")
                         .content(requestBody))
                 .andDo(print())
