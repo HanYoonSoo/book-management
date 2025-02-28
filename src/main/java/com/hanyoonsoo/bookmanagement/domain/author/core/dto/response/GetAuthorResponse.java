f2075017809c149341ea49e2fbc1cda9f520f80c
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-28 10:31:30 +0900

    [#16] docs: 저자 응답 DTO Schema 문서 작성

--- src/main/java/com/hanyoonsoo/bookmanagement/domain/author/core/dto/response/GetAuthorResponse.java
+++ src/main/java/com/hanyoonsoo/bookmanagement/domain/author/core/dto/response/GetAuthorResponse.java
@@ -1,17 +1,21 @@
 package com.hanyoonsoo.bookmanagement.domain.author.core.dto.response;
 
 import com.hanyoonsoo.bookmanagement.domain.author.core.entity.Author;
-import lombok.Builder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
-@Builder
+@SuperBuilder
 public class GetAuthorResponse {
 
+    @Schema(description = "저자 Id", example = "1")
     private Long authorId;
 
+    @Schema(description = "저자 이름", example = "홍길동")
     private String name;
 
+    @Schema(description = "저자 이메일", example = "test@example.com")
     private String email;
 
     public static GetAuthorResponse from(Author author){
